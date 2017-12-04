2877e48f05c0c58a96b6467b49f43baa79dc24ee
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2017-12-04 20:03:26 +0100

    Minors

--- PyDG1D/dgtd.py
+++ PyDG1D/dgtd.py
@@ -5,52 +5,48 @@ Created on Nov 23, 2017
 '''
 
 import numpy as np
+import scipy.special
 
 if __name__ == '__main__':
     pass
 
-def meshGrid(D, N, vertices):
+
+def setNodes1D(N, vertices):
     """ 
-    Sets N+1 nodes in equispaced positions using the vertices indicated by vx.
+    Sets N+1 nodes in equispaced positions using the vertices indicated
+    by vx.
     """
-    if D != 1:
-        raise ValueError("Not implemented")
     
     K = vertices.shape[1]
-    x = np.zeros( (N+1, K) )
+    x = np.zeros((N+1, K))
     for k in range(K):
         for i in range(N+1):
-            x[i,k] = i * (vertices[1,k] -vertices[0,k]) / N + vertices[0,k];
+            x[i,k] = i * (vertices[1,k] - vertices[0,k]) / N + vertices[0,k];
             
     return x
 
-def nodeIndices(D, N):
+
+def nodeIndices(N):
     """
     Generates number of node Indices for order N and dimension D
     """
-    if D != 1:
-        raise ValueError("Not implemented")
-    
     Np = N+1;
-    nId = np.array(Np, D+1)
+    nId = np.array(Np, 2)
     for i in range(N):
-        nId[[i,0]] = N-i
-        nId[[i,1]] = i
-    
+        nId[i] = [N-i, i]     
     return nId
     
-
-def silvesterPolynomial(D, N):
     
-
-def lagrangePolynomial(D, N):
-
-
-def massMatrix(D, N):
+def jacobiGL(alpha, beta, N):
     """
-    Creates mass matrix analitically for given dimension and order.
-    """
-    if dimension != 1:
-        raise ValueError("Not implemented")
+    Compute the order N Gauss Lobatto quadrature points, x, associated
+    with the Jacobi polynomial.
+    """     
+    if N==0:
+        return np.array([0.0])
+    if N==1:
+        return np.array([-1.0, 1.0])
     
-    N = order;
+    x = scipy.special.roots_jacobi(N-2, alpha+1, beta+1);
+    return x
+    
