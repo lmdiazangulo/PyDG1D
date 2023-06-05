f4008cfdaba68c5e0e30c658ac60c73eb90d1a91
FarahAmador <82028664+FarahAmador@users.noreply.github.com>
2023-06-05 09:28:15 +0200

    changing IRK4, NOT WORKING

--- dgtd/IRK4.py
+++ dgtd/IRK4.py
@@ -3,15 +3,6 @@ import numpy as np
 from .spatialDiscretization import *
 
 class IRK4:
-    A = np.array([
-        0,	-0.417890474499852,	-1.19215169464268,	-1.69778469247153,	-1.51418344425716
-    ])
-    B = np.array([
-        0.149659021999229,	0.379210312999627, 0.822955029386982,	0.699450455949122,	0.153057247968152
-    ])
-    C = np.array([
-        0,	0.149659021999229,	0.370400957364205, 0.622255763134443,	0.958282130674690
-    ])
 
     N_STAGES = 2
 
@@ -41,8 +32,9 @@ class IRK4:
             dif = 1 
             x0 = fields
             while i < maxiter and dif > tol:
+                self.sp.computeRHS(fields)-
                 [fx0, dfx0] = f(t[k], x0)
-                g = x0 - y[k-1] - h/24*(f(t[k-3],y[k-3])[0] -5*f(t[k-2],y[k-2])[0]+ 19*f(t[k-1],y[k-1])[0] +9*fx0)
+                g = x0 - fields - dt/24*(f(t[k-3],y[k-3])[0] -5*f(t[k-2],y[k-2])[0]+ 19*f(t[k-1],y[k-1])[0] +9*fx0)
                 dg = 1-h/24*9*dfx0
                 x1 = x0-g/dg
                 dif=abs(x1-x0)
