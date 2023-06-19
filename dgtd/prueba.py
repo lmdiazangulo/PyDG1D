29a07ea62da9a358290e212d0bf72f524dc8f636
FarahAmador <82028664+FarahAmador@users.noreply.github.com>
2023-06-19 10:45:06 +0200

    changing IGRK4 not working now

--- dgtd/prueba.py
+++ dgtd/prueba.py
@@ -1,10 +1,1 @@
-from scipy.optimize import fsolve
-import math
 
-def equations(p):
-    x, y = p
-    return (x+y**2-4, math.exp(x) + x*y - 3)
-
-x, y =  fsolve(equations, (1, 1))
-
-print ((x, y))
