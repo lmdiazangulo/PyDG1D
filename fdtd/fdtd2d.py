d9a84a1d0b7fa556e34bec26858898427bb057d0
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2024-04-08 12:37:39 +0200

    Now passes tests

--- fdtd/fdtd2d.py
+++ fdtd/fdtd2d.py
@@ -10,15 +10,22 @@ else:
 
 
 class FDTD2D(SpatialDiscretization):  # TE mode
-    def __init__(self, x_min, x_max, kx_elem, y_min=0.0, y_max=0.0, ky_elem=0, boundary_label="PEC"):
+    def __init__(self, x_min, x_max, kx_elem, y_min=0.0, y_max=0.0, ky_elem=0, boundary_labels="PEC"):
+        if type(boundary_labels) == str:
+            self.boundary_labels = dict()
+            self.boundary_labels["XL"] = boundary_labels
+            self.boundary_labels["XU"] = boundary_labels
+            self.boundary_labels["YL"] = boundary_labels
+            self.boundary_labels["YU"] = boundary_labels
+        else:
+            self.boundary_labels = boundary_labels
+
         if y_min == 0.0 and y_max == 0.0 and ky_elem == 0:
             y_min = x_min
             y_max = x_max
             ky_elem = kx_elem
         else:
             raise ValueError("Invalid values for y grid planes.")
-
-        self.boundary_label = boundary_label
 
         self.x = np.linspace(x_min, x_max, num=kx_elem+1)
         self.y = np.linspace(y_min, y_max, num=ky_elem+1)
@@ -52,22 +59,32 @@ class FDTD2D(SpatialDiscretization):  # TE mode
         rhsEx = np.zeros(Ex.shape)
         rhsEy = np.zeros(Ey.shape)
 
-        rhsEx[1:-1, :] = - self.cEy * (H[:-1, :] - H[1:, :])
-        rhsEy[:, 1:-1] = - self.cEx * (H[:, :-1] - H[:, 1:])
+        rhsEx[1:-1, :] =   self.cEy * ( H[1:, :] - H[:-1, :])
+        rhsEy[:, 1:-1] = - self.cEx * ( H[:, 1:] - H[:, :-1])
 
-        if self.boundary_label == "PEC":
-            rhsEx[0, :] = 0.0
-            rhsEx[-1, :] = 0.0
-            rhsEy[:,  0] = 0.0
-            rhsEy[:, -1] = 0.0
-        
-        if self.boundary_label == "PMC":
-        
-            rhsEx[0, :] =  - self.cEy * (2*H[0,:])
-            rhsEx[-1, :] =  - self.cEy * (-2*H[-1,:])
-
-            rhsEy[:, 0] =  - self.cEx * (2*H[:,0])
-            rhsEy[:, -1] =  - self.cEx * (-2*H[:,-1])
+        for bdr, label in self.boundary_labels.items():
+            if bdr == "XL":
+                if label == "PEC":
+                    rhsEy[:,  0] = 0.0
+                elif label == "PMC":
+                    rhsEy[:, 0] =  - self.cEx * (2*H[:,0])
+            elif bdr == "XU":
+                if label == "PEC":
+                    rhsEy[:, -1] = 0.0
+                elif label == "PMC":
+                    rhsEy[:, -1] =  - self.cEx * (-2*H[:,-1])
+            elif bdr == "YL":
+                if label == "PEC":
+                    rhsEx[0, :] = 0.0
+                elif label == "PMC":
+                    rhsEx[0, :] =  self.cEy * (2*H[0,:])
+            elif bdr == "YU":
+                if label == "PEC":
+                    rhsEx[-1, :] = 0.0
+                elif label == "PMC":
+                    rhsEx[-1, :] =  self.cEy * (-2*H[-1,:])
+            else:
+                raise ValueError("Invalid boundary tag.")       
 
         return {'x': rhsEx, 'y': rhsEy}
 
@@ -75,8 +92,8 @@ class FDTD2D(SpatialDiscretization):  # TE mode
         Ex = fields['E']['x']
         Ey = fields['E']['y']
 
-        rhsH = - self.cEy*(Ex[1:, :] - Ex[:-1, :]) \
-               + self.cEx*(Ey[:, 1:] - Ey[:, :-1])
+        rhsH = + self.cEy*(Ex[1:, :] - Ex[:-1, :]) \
+               - self.cEx*(Ey[:, 1:] - Ey[:, :-1])
 
         return rhsH
 
--- test/test_maxwellDriver2d.py
+++ test/test_maxwellDriver2d.py
@@ -41,18 +41,45 @@ def test_pec():
     R = np.corrcoef(ez_expected, driver['Ez'])
     assert R[0,1] > 0.9
 
-def test_fdtd2d_te_pec():
-    sp = FDTD2D(x_min=-1.0, x_max=1.0, kx_elem=100, boundary_label="PEC")
+def test_fdtd2d_te_pec_x():
+    sp = FDTD2D(x_min=-1.0, x_max=1.0, kx_elem=100, boundary_labels="PEC")
     driver = MaxwellDriver(sp, timeIntegratorType='LF2', CFL=1.0)
     s0 = 0.25
     final_time = 2.0
 
     xH, yH = np.meshgrid(sp.xH, sp.yH)
-    initialFieldH = np.exp(-xH**2/(2*s0**2)) #no seria test PMC esto??
+    initialFieldH = np.exp(-xH**2/(2*s0**2))
     driver['H'][:,:] = initialFieldH[:,:]
 
     # while driver.timeIntegrator.time < final_time:
     #     plt.contourf(xH, yH, driver['H'], vmin=-1.0, vmax=1.0)
+    #     plt.plot(sp.x, driver['E']['y'][4,:])
+    #     plt.plot(sp.xH, driver['H'][4,:])
+    #     plt.ylim(-1, 1)
+    #     plt.grid(which='both')
+    #     plt.pause(0.01)
+    #     plt.cla()
+    #     driver.step()
+
+    driver.run_until(final_time)
+
+    finalFieldH = driver['H']
+    R = np.corrcoef(initialFieldH.ravel(), finalFieldH.ravel())
+    assert R[0, 1] > 0.9999
+
+def test_fdtd2d_te_pec_y():
+    sp = FDTD2D(x_min=-1.0, x_max=1.0, kx_elem=100, boundary_labels="PEC")
+    driver = MaxwellDriver(sp, timeIntegratorType='LF2', CFL=1.0)
+    s0 = 0.25
+    final_time = 2.0
+
+    xH, yH = np.meshgrid(sp.xH, sp.yH)
+    initialFieldH = np.exp(-yH**2/(2*s0**2))
+    driver['H'][:,:] = initialFieldH[:,:]
+
+    # while driver.timeIntegrator.time < final_time:
+    #     plt.contourf(xH, yH, driver['H'], vmin=-1.0, vmax=1.0)
+    #     plt.plot(sp.x, driver['E']['y'][4,:])
     #     plt.plot(sp.xH, driver['H'][4,:])
     #     plt.ylim(-1, 1)
     #     plt.grid(which='both')
@@ -67,9 +94,14 @@ def test_fdtd2d_te_pec():
     assert R[0, 1] > 0.9999
 
 
-
-def test_fdtd2d_te_pmc():
-    sp = FDTD2D(x_min=-1.0, x_max=1.0, kx_elem=100, boundary_label="PMC")
+def test_fdtd2d_te_pmc_x():
+    bdrs = {
+        "XL": "PMC",
+        "XU": "PMC",
+        "YL": "PEC",
+        "YU": "PEC"
+    }
+    sp = FDTD2D(x_min=-1.0, x_max=1.0, kx_elem=100, boundary_labels=bdrs)
     driver = MaxwellDriver(sp, timeIntegratorType='LF2', CFL=1.00)
 
     s0 = 0.25
@@ -81,6 +113,7 @@ def test_fdtd2d_te_pmc():
 
     # while driver.timeIntegrator.time < final_time:
     #     plt.contourf(xH, yH, driver['H'], vmin=-1.0, vmax=1.0)
+    #     plt.plot(sp.x, driver['E']['y'][4,:])
     #     plt.plot(sp.xH, driver['H'][4,:])
     #     plt.ylim(-1, 1)
     #     plt.grid(which='both')
@@ -92,4 +125,36 @@ def test_fdtd2d_te_pmc():
 
     finalFieldH = driver['H']
     R = np.corrcoef(initialFieldH.ravel(), -finalFieldH.ravel())
-    #assert R[0, 1] > 0.9999
+    assert R[0, 1] > 0.9999
+
+
+def test_fdtd2d_te_pmc_y():
+    bdrs = {
+        "XL": "PEC",
+        "XU": "PEC",
+        "YL": "PMC",
+        "YU": "PMC"
+    }
+    sp = FDTD2D(x_min=-1.0, x_max=1.0, kx_elem=100, boundary_labels=bdrs)
+    driver = MaxwellDriver(sp, timeIntegratorType='LF2', CFL=1.0)
+
+    s0 = 0.25
+    final_time = 2.0
+
+    xH, yH = np.meshgrid(sp.xH, sp.yH)
+    initialFieldH = np.exp(-yH**2/(2*s0**2))
+    driver['H'][:,:] = initialFieldH[:,:]
+
+    # while driver.timeIntegrator.time < final_time:
+    #     plt.contourf(xH, yH, driver['H'], vmin=-1.0, vmax=1.0)
+    #     plt.ylim(-1, 1)
+    #     plt.grid(which='both')
+    #     plt.pause(0.01)
+    #     plt.cla()
+    #     driver.step()
+
+    driver.run_until(final_time)
+
+    finalFieldH = driver['H']
+    R = np.corrcoef(initialFieldH.ravel(), -finalFieldH.ravel())
+    assert R[0, 1] > 0.9999
